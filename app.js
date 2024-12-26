697e008c356f1fb6f57d7e233919e46df4bee3fb
imane <imanezenasnii@gmail.com>
2024-12-26 10:07:34 +0100

    Add user router with POST /create endpoint

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
+// Set up an Express server and the user router
 require('./db');
 require('dotenv').config();
 const express = require('express');
+const userRouter = require('./routes/user');
 
 const app = express();
 
 app.use(express.json());
+app.use('/api/user/', userRouter);
 
 const PORT = process.env.PORT || 4000;
 
