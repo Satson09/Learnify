3b1c05e638e263420f36d79cd8021a80c30997fb
imane <imanezenasnii@gmail.com>
2024-12-26 07:24:26 +0100

    Set up Express server with basic config

---
+++ app.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+require('./db');
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`);
+});
