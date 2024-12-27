01246ef57604a09a89e511211c9d0d2fa4daac73
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-12-27 21:57:59 +0200

    function to interacte with courses Collection

---
+++ utils/coursesOp.js
@@ -0,0 +1,52 @@
+import Courses from '../models/courses.js';
+
+async function createCourse(options = {}) {
+  try {
+    const result = await Courses.create(options);
+    return result;
+  } catch (error) {
+    console.log('error is because of :', error);
+    return null;
+  }
+}
+
+async function searchCourse(options = {}) {
+  // search or find a function on any parameter/s you want
+  try {
+    const result = await Courses.find(options);
+    return result;
+  } catch (error) {
+    console.log('error is : ', error);
+    return null;
+  }
+}
+
+async function updCourse(ObjectId, updatedfield) {
+  // update a function based on the _ID
+  try {
+    // const objectid = new mongoose.Schema.Types.ObjectId(_id);
+    const result = await Courses.findByIdAndUpdate(ObjectId, updatedfield, { new: true });
+    return result;
+  } catch (err) {
+    console.log('Error becuase of :', err);
+    return null;
+  }
+}
+
+async function delCourse(ObjectId) {
+  // delete base on the _ID
+  try {
+    const result = await Courses.findByIdAndDelete(ObjectId);
+    return result;
+  } catch (error) {
+    console.log('Error because of : ', error);
+    return null;
+  }
+}
+
+export default {
+  delCourse,
+  updCourse,
+  createCourse,
+  searchCourse,
+};
