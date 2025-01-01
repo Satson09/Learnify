8ea67165c33a5f1d3bcc2670eda94d0c326731f3
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2025-01-01 14:35:49 +0200

    sumbit and course-progress routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import ProgressCalculation from '../controllers/ProgressController.js'
 import { createUser } from '../controllers/user.js';
 import { validateUser, validate } from '../middlewares/validator.js';
 
 const router = express.Router();
+
 router.post('/create', validateUser, validate, createUser);
+
+router.get('/course-progress/:studentId/:courseId', ProgressCalculation.GetProg);
+
+router.post('/sumbit-quize', ProgressCalculation.PostProg);
 
 export default router;
