5dad171859a7ba45574261fc97217ddad7cda7e4
satson09 <satsunjohnson@gmail.com>
2025-01-23 15:48:45 +0100

    app API routes

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,13 +1,3 @@
-/**
-const express = require('express');
-const {createUser} = require('../controllers/user');
-const { validateUser, validate } = require('../middlewares/validator');
-
-const router = express.Router();
-router.post('/create', validateUser, validate, createUser);
-
-module.exports = router;
-*/
 
 const express = require('express');
 const { createUser, loginUser } = require('../../controllers/user'); // Import user controller
