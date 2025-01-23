8af636cfe4da429708f07305f4a166b00969b8cf
satson09 <satsunjohnson@gmail.com>
2025-01-23 15:42:22 +0100

    connect DB with mongoose handler

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// Connect to MongoDB database 'myDb' using Mongoose
+// Connecting to MongoDB database 'myDb' using Mongoose
 mongoose.connect('mongodb://localhost:27017/Learnify', {}).then(() => {
   console.log('DB is connected');
 }).catch((err) => {
