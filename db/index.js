7e196691bc2da061d8d8bedc58113029afe6578d
satson09 <satsunjohnson@gmail.com>
2025-01-27 17:56:41 +0100

    connecting mongoose to db

--- db/index.js
+++ db/index.js
@@ -10,14 +10,3 @@ mongoose.connect('mongodb://localhost:27017/Learnify', {})
 
 
 
-
-/**
-const mongoose = require('mongoose');
-
-// Connecting to MongoDB database 'myDb' using Mongoose
-mongoose.connect('mongodb://localhost:27017/Learnify', {}).then(() => {
-  console.log('DB is connected');
-}).catch((err) => {
-  console.log(err);
-});
-*/
